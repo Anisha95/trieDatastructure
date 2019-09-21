55b4ff5f703169f214c2fcf84eb2bc754faab1c8
Anisha Pal <pal.anisha26@gmail.com>
2019-09-21 20:40:08 +0530

    file added

---
+++ trie.py
@@ -0,0 +1,32 @@
+head = {} #root node
+
+
+def add(word):
+	cur = head;
+	for ch in word:
+		if ch not in cur:
+			cur[ch] = {} #starting to create nodes
+		cur = cur[ch]
+	cur['*'] = True
+		
+
+def search(word):
+	cur = head;
+	for ch in word:
+		if ch not in cur:
+			return False
+		cur = cur[ch]
+	if '*' in cur:
+		return True
+	else:
+		return False
+		
+		
+
+
+
+add('hello')
+add('hella')
+add('helicopter')
+print(search('hello'))
+print(search('hellay'))
